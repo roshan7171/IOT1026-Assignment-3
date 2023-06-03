24ae5c2e2cbd85aff7be346286f5431dfd13e02b
Roshan Karki <A00276018@mycambrian.ca>
2023-06-03 15:34:53 -0400

    test completed

--- Assignment/Pack.cs
+++ Assignment/Pack.cs
@@ -1,87 +1,147 @@
-﻿namespace Assignment;
-
-public class Pack
+﻿namespace Assignment
 {
-    private readonly InventoryItem[] _items; // You can use another data structure here if you prefer.
-    // You may need another private member variable if you use an array data structure.
-
-    private readonly int _maxCount;
-    private readonly float _maxVolume;
-    private readonly float _maxWeight;
-
-    // Default constructor sets the maxCount to 10 
-    // maxVolume to 20 
-    // maxWeight to 30
-    public Pack() : this(10, 20, 30) { }
-
-    // This constructor is not complete, but it is a good start.
-    public Pack(int maxCount, float maxVolume, float maxWeight)
+    public class Pack
     {
-        _maxCount = maxCount;
-        _maxVolume = maxVolume;
-        _maxWeight = maxWeight;
-    }
+        private readonly InventoryItem[] _items;
+        private readonly int _maxCount;
+        private readonly float _maxVolume;
+        private readonly float _maxWeight;
+        private int _currentCount;
+        private float _currentVolume;
+        private float _currentWeight;
 
-    // This is called a getter
-    public int GetMaxCount()
-    {
-        return _maxCount;
-    }
+        public Pack() : this(10, 20, 30) { }
 
-    public bool Add(InventoryItem item)
-    {
-        float weight = item.GetWeight();
-        throw new NotImplementedException();
-    }
-
-    // Implement this class
-    public override string ToString()
-    {
-        throw new NotImplementedException();
-    }
-}
-
-// Come back to this once we learn about abstract classes.
-public class InventoryItem
-{
-    private readonly float _volume;
-    private readonly float _weight;
-
-    public InventoryItem(float volume, float weight)
-    {
-        if (volume <= 0f || weight <= 0f)
+        public Pack(int maxCount, float maxVolume, float maxWeight)
         {
-            throw new ArgumentOutOfRangeException($"An item can't have {volume} volume or {weight} weight");
+            _items = new InventoryItem[maxCount];
+            _maxCount = maxCount;
+            _maxVolume = maxVolume;
+            _maxWeight = maxWeight;
         }
-        _volume = volume;
-        _weight = weight;
+
+        public int GetMaxCount()
+        {
+            return _maxCount;
+        }
+
+        public float GetVolume()
+        {
+            return _currentVolume;
+        }
+
+        public bool Add(InventoryItem item)
+        {
+            float weight = item.GetWeight();
+            float volume = item.GetVolume();
+            
+           
+            if (_currentCount < _maxCount && _currentWeight + weight <= _maxWeight && _currentVolume + volume <= _maxVolume)
+            {
+                _currentWeight += weight;
+                _currentVolume += volume;
+                _items[_currentCount++] = item;
+                return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string packContents = "";
+            for (int i = 0; i < _currentCount; i++)
+            {
+                packContents += _items[i].Display() + "\n";
+            }
+            return packContents;
+        }
     }
 
-    // Getters
-    public float GetVolume()
+    public abstract class InventoryItem
     {
-        return _volume;
+        private readonly float _volume;
+        private readonly float _weight;
+
+        protected InventoryItem(float volume, float weight)
+        {
+            if (volume <= 0f || weight <= 0f)
+            {
+                throw new ArgumentOutOfRangeException($"An item can't have {volume} volume or {weight} weight");
+            }
+            _volume = volume;
+            _weight = weight;
+        }
+
+        public abstract string Display();
+
+        public float GetVolume()
+        {
+            return _volume;
+        }
+
+        public float GetWeight()
+        {
+            return _weight;
+        }
     }
 
-    public float GetWeight()
+    public class Arrow : InventoryItem
     {
-        return _weight;
+        public Arrow() : base(0.5f, 0.1f) { }
+
+        public override string Display()
+        {
+            return $"An arrow with weight {GetWeight()} and volume {GetVolume()}";
+        }
+    }
+
+    public class Bow : InventoryItem
+    {
+        public Bow() : base(1f, 4f) { }
+
+        public override string Display()
+        {
+            return $"A bow with weight {GetWeight()} and volume {GetVolume()}";
+        }
+    }
+
+    public class Rope : InventoryItem
+    {
+        public Rope() : base(1f, 1.5f) { }
+
+        public override string Display()
+        {
+            return $"A rope with weight {GetWeight()} and volume {GetVolume()}";
+        }
+    }
+
+    public class Water : InventoryItem
+    {
+        public Water() : base(2f, 3f) { }
+
+        public override string Display()
+        {
+            return $"Water with weight {GetWeight()} and volume {GetVolume()}";
+        }
+    }
+
+    public class Food : InventoryItem
+    {
+        public Food() : base(1f, 0.5f) { }
+
+        public override string Display()
+        {
+            return $"Yummy food with weight {GetWeight()} and volume {GetVolume()}";
+        }
+    }
+
+    public class Sword : InventoryItem
+    {
+        public Sword() : base(5f, 3f) { }
+
+        public override string Display()
+        {
+            return $"A sharp sword with weight {GetWeight()} and volume {GetVolume()}";
+        }
     }
 }
-
-// Implement these classes - each inherits from InventoryItem
-// 1 line of code each - call base class constructor with appropriate arguments
-class Arrow : InventoryItem
-{
-    public Arrow() : base(0.05f, 0.1f) { }
-}
-
-class Bow : InventoryItem
-{
-    public Bow() : base(1f, 4f) { }
-}
-
-class Rope : InventoryItem { }
-class Water : InventoryItem { }
-class Food : InventoryItem { }
-class Sword : InventoryItem { }
